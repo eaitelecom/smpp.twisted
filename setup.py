02f094fb09a4b1638f8f9de13b8ca7b98def39e9
Adailton Nascimento <dhelbegor@gmail.com>
2023-08-14 20:32:53 -0300

    add support for python 3.8+

--- setup.py
+++ setup.py
@@ -1,34 +1,35 @@
-import os
 from setuptools import setup, find_packages
 
-def read(fname):
-    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-from setuptools import setup, find_packages
+with open("README.markdown", "r", encoding="utf-8") as readme_file:
+    long_description = readme_file.read()
+
+
 setup(
-    name = "smpp.twisted",
-    version = "0.4",
-    author = "Roger Hoover",
-    author_email = "roger.hoover@gmail.com",
-    description = "SMPP 3.4 client built on Twisted",
-    license = 'Apache License 2.0',
-    packages = find_packages(),
-    long_description=read('README.markdown'),
-    keywords = "smpp twisted",
-    url = "https://github.com/mozes/smpp.twisted",
+    name="smpp.twisted",
+    version="0.4",
+    author="Roger Hoover",
+    author_email="roger.hoover@gmail.com",
+    description="SMPP 3.4 client built on Twisted",
+    license="Apache License 2.0",
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    keywords="smpp twisted",
+    url="https://github.com/eaitelecom/smpp.twisted",
     py_modules=["smpp.twisted"],
-    include_package_data = True,
-    package_data={'smpp.twisted': ['README.markdown']},
-    zip_safe = False,   
-    install_requires = [
+    include_package_data=True,
+    package_data={"smpp.twisted": ["README.markdown"]},
+    zip_safe=False,
+    install_requires=[
         'twisted',
         'pyOpenSSL',
-        'smpp.pdu',
+        'git+https://github.com/eaitelecom/smpp.pdu.git',
     ],
-    tests_require = [
+    tests_require=[
         'mock',
     ],
-    test_suite = 'smpp.twisted.tests',
+    test_suite='smpp.twisted.tests',
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Framework :: Twisted",
@@ -37,7 +38,8 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Intended Audience :: Developers",
         "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
         "Topic :: Software Development :: Libraries :: Python Modules",
     ],
 )
-
